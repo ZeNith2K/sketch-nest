5afae2dd5a3e64ce72918b1027cf6060a47f50c5
Sumit Kumar <sumitpks99@gmail.com>
2024-11-26 21:43:21 +0530

    fix: control-panel stying

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -6,7 +6,7 @@ const ControlPanel = () => {
   const setSelectedTool = useStore((state) => state.setSelectedTool)
 
   return (
-    <div className="border shadow rounded-md py-2 px-5 absolute top-10 left-0 right-0 m-auto w-[60vw] z-10 flex items-center justify-start gap-5">
+    <div className="border shadow rounded-md py-2 px-5 absolute top-10 left-0 right-0 m-auto w-[60vw] z-10 bg-white flex items-center justify-start gap-5">
       <div className='p-1 rounded hover:bg-slate-100'>
         <Square size={22} onClick={() => setSelectedTool('square')} weight={ selectedTool == 'square' ? 'fill' : 'light'} className='text-slate-800 cursor-pointer'/>
       </div>
